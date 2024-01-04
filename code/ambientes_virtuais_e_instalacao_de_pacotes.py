6dec679c8a54cc60040a90f07dfaa5f8047939ea
Luca Clemente <clementluca6@gmail.com>
2024-01-04 20:11:48 -0300

    Como usar o venv

---
+++ code/ambientes_virtuais_e_instalacao_de_pacotes.py
@@ -0,0 +1,7 @@
+#Para a criação de um venv basta digitar:
+#python3 -m venv <nomedovenv>
+#Para acessar/ativar o seu venv é necessário:
+#source <nomedovenv>/bin/activate
+#Para desativar o seu venv basta digitar:
+#deactivate
+
