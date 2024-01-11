c2142fe53ede2550f25719359a839301c8fa22ef
Luca Clemente <clementluca6@gmail.com>
2024-01-10 22:22:22 -0300

    documentação e post route

--- code/api.py
+++ code/api.py
@@ -1,9 +1,21 @@
-from flask import Flask
+from flask import Flask,request
+from flask_pydantic_spec import FlaskPydanticSpec,Request
 
 server_app = Flask(__name__)
+spec = FlaskPydanticSpec('flask',title = 'Um pouco sobre apis')
+spec.register(server_app)
 
 @server_app.get('/recurso')
 def pegar_algo():
     return 'Estou pegando'
+
+@server_app.post('/pessoas')
+@spec.validate(
+    body=Request(Pessoa), resp=Response(HTTP_200=Pessoa)
+)
+def inserir_pessoa():
+    """Insere uma Pessoa no banco de dados."""
+    body = request.context.body.dict()
+    return body
 
 server_app.run()
