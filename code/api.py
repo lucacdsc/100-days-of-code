9f8ead110089e052bee0976ca727dec0e7f5b1ff
Luca Clemente <clementluca6@gmail.com>
2024-01-13 23:44:39 -0300

    inserindo dados no database tinydb

--- code/api.py
+++ code/api.py
@@ -1,10 +1,12 @@
-from flask import Flask,request
+from flask import Flask,request,jsonify
 from flask_pydantic_spec import FlaskPydanticSpec,Request,Response
 from pydantic import BaseModel
+from tinydb import TinyDB,Query
 
 server_app = Flask(__name__)
 spec = FlaskPydanticSpec('flask',title = 'Um pouco sobre apis')
 spec.register(server_app)
+database = TinyDB('database.json')
 
 class Pessoa(BaseModel):
     id: int
@@ -14,7 +16,8 @@ class Pessoa(BaseModel):
 @server_app.get('/pessoas')
 @spec.validate(resp=Response(HTTP_200=Pessoa))
 def pegar_algo():
-    return 'Estou pegando'
+    """Retorna todas as pessoas da base de dados."""
+    return jsonify(database.all())
 
 @server_app.post('/pessoas')
 @spec.validate(
@@ -23,6 +26,7 @@ def pegar_algo():
 def inserir_pessoa():
     """Insere uma Pessoa no banco de dados."""
     body = request.context.body.dict()
+    database.insert(body)
     return body
 
 server_app.run()
