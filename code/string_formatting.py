8bbb98ac11cd767a6e04083d45880c500c1b0451
Luca Clemente <clementluca6@gmail.com>
2024-01-22 20:36:03 -0300

    formatação de strings

---
+++ code/string_formatting.py
@@ -0,0 +1,13 @@
+gon,killua,nen = 'gosto', 'muito', 1
+
+print('%s %s %s' %(gon,killua,nen))
+print('{} {} {}'.format(gon,killua,nen))
+print(f'{gon} {killua} {nen}')
+
+#tipos de formatação existentes 
+
+
+print('{nome}'.format(nome='Luca'))
+print('Opa {nome} {sobrenome}, tudo bem?'.format(nome='Gon', sobrenome='Freecs'))
+
+print(f'{nen.upper()}')
