c099b8788374dee3c7ec5d32168a43c42894182a
Luca Clemente <clementluca6@gmail.com>
2024-01-23 21:14:11 -0300

    More examples

--- code/string_formatting.py
+++ code/string_formatting.py
@@ -1,4 +1,4 @@
-gon,killua,nen = 'gosto', 'muito', 1
+gon,killua,nen = 'gosto', 'muito', 'hxh'
 
 print('%s %s %s' %(gon,killua,nen))
 print('{} {} {}'.format(gon,killua,nen))
@@ -11,3 +11,18 @@ print('{nome}'.format(nome='Luca'))
 print('Opa {nome} {sobrenome}, tudo bem?'.format(nome='Gon', sobrenome='Freecs'))
 
 print(f'{nen.upper()}')
+
+nome = 'ração'
+print(f'{nome.upper()=!a}')
+
+print(len(f'{nome:20}'))
+
+nomes = ['Luca','Puro Osso','Billy']
+idades = [22, 999, 13]
+
+for nome,idade in zip(nomes,idades):
+    print(f'{nome:9} {idade}')
+
+
+gatinho =  'Chiclete'
+print(f'{gatinho:*^10}')
